23b8a4cab0919546b36d0f98c155111413d3a31a
PC-11 <PC-11@DESKTOP-GLVII3M>
2024-02-05 21:23:55 +0900

    20240205

---
+++ svnTest/src/kr/or/ddit/basic/HSJ.java
@@ -0,0 +1,8 @@
+package kr.or.ddit.basic;
+
+public class HSJ {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("test");
+	}
+}
---
+++ svnTest/src/kr/or/ddit/basic/jmw.java
@@ -0,0 +1,10 @@
+package kr.or.ddit.basic;
+
+public class jmw {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world !!");
+		System.out.println("fucking world !!");
+	}
+
+}
